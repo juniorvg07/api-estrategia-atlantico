d6e5be1234614a9f370a7394ffd50a4996eeb74d
Junior Vasquez <junior.vasquez07@gmail.com>
2025-05-19 09:54:33 -0500

    Actualizando direcciones en CORS

--- src/main/java/com/vasco/referidos/auth/SecurityConfig.java
+++ src/main/java/com/vasco/referidos/auth/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://juniorvg07.github.io/estrategia-atlantico/", "https://estrategia-atlantico.web.app/"));  // Tu frontend
+        config.setAllowedOrigins(List.of("https://juniorvg07.github.io", "https://estrategia-atlantico.web.app"));  // Tu frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Necesario si usas cookies o Authorization headers
